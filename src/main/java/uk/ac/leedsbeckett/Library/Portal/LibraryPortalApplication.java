cc78e89dab0afc9749b41152631cc09b00eab263
Sds0702 <seq.sam@gmail.com>
2023-04-03 20:43:50 +0100

    first dev commit

---
+++ src/main/java/uk/ac/leedsbeckett/Library/Portal/LibraryPortalApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.leedsbeckett.Library.Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/leedsbeckett/Library/Portal/LibraryPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.leedsbeckett.Library.Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
