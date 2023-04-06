2b362c98ae4704a35e949d650f8606c3954a08fc
Sds0702 <seq.sam@gmail.com>
2023-04-06 21:32:12 +0100

    third dev commit

--- src/main/java/uk/ac/leedsbeckett/Library/Portal/LibraryPortalApplication.java
+++ src/main/java/uk/ac/leedsbeckett/Library/Portal/LibraryPortalApplication.java
@@ -3,7 +3,7 @@ package uk.ac.leedsbeckett.Library.Portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //provides component scanning, autoconfiguration and property support
 public class LibraryPortalApplication {
 
 	public static void main(String[] args) {
